ea9501dcfe04cafebd4f035bb98fa09efb39bf07
Alan Krueger <alan@trigonic.com>
2011-07-13 16:49:30 -0500

    refactor XmlResourceApplicationContext to be more flexible about its construction arguments

--- src/main/java/com/trigonic/utils/spring/context/XmlResourceApplicationContext.java
+++ src/main/java/com/trigonic/utils/spring/context/XmlResourceApplicationContext.java
@@ -1,8 +1,13 @@
 package com.trigonic.utils.spring.context;
 
+import java.io.File;
+import java.net.URL;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 
 /**
  * Surprisingly, Spring doesn't seem to have an {@link AbstractXmlApplicationContext} that pulls from a set of
@@ -14,18 +19,42 @@ import org.springframework.core.io.Resource;
 public class XmlResourceApplicationContext extends AbstractXmlApplicationContext {
     private final Resource[] configResources;
 
-    public XmlResourceApplicationContext(Resource... configResources) {
-        this(configResources, null);
+    public XmlResourceApplicationContext(Object... configResources) {
+        this(null, configResources);
     }
 
-    public XmlResourceApplicationContext(Resource[] configResources, ApplicationContext parent) {
+    public XmlResourceApplicationContext(ApplicationContext parent, Object... configResources) {
         super(parent);
-        this.configResources = configResources;
+        this.configResources = normalize(configResources);
         refresh();
     }
 
     @Override
     protected Resource[] getConfigResources() {
         return configResources;
+    }
+    
+    private Resource[] normalize(Object[] resources) {
+        Resource[] results = new Resource[resources.length];
+        for (int i = 0; i < resources.length; ++i) {
+            results[i] = normalize(resources[i]);
+        }
+        return results;
+    }
+
+    private Resource normalize(Object resource) {
+        Resource result;
+        if (resource instanceof Resource) {
+            result = (Resource) resource;
+        } else if (resource instanceof String) {
+            result = getResource((String) resource);
+        } else if (resource instanceof File) {
+            result = new FileSystemResource((File) resource);
+        } else if (resource instanceof URL) {
+            result = new UrlResource((URL) resource);
+        } else {
+            throw new IllegalArgumentException("unable to convert " + resource.getClass().getName() + " to " + Resource.class.getName());
+        }
+        return result;
     }
 }
